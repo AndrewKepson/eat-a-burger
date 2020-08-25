c82b4c636fb2cb4420b5d1b9c34233d1d72f26b5
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-25 09:45:36 -0600

    Updating server and packages.

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,29 @@
-const orm = require('../config/orm');
+const orm = require("../config/orm");
 
 const burger = {
-	allBurgers: (cb) => {
-		orm.selectAll('burgers', (data) => {
-			cb(data);
-		});
-	},
+    showBurgers: (cb) => {
+        orm.selectAll("burgers", data => {
+            cb(data);
+        });
+    },
 
-	createBurger: (insertData, cb) => {
-		orm.insertOne('burgers', insertData, (result) => {
-			cb(result);
-		});
-	},
+    addBurger: (insertData, cb) => {
+        orm.insertOne("burgers", insertData, result => {
+            cb(result);
+        })
+    },
 
-	updateBurger: (updateData, condition, cb) => {
-		orm.updateOne('burgers', updateData, condition, (result) => {
-			cb(result);
-		});
-	},
+    updateBurger: (updateData, condition, cb) => {
+        orm.updateOne("burgers", updateData, condition, result => {
+            cb(result);
+        })
+    },
 
-	deleteBurger: (condition, cb) => {
-		orm.deleteOne('burgers', condition, (result) => {
-			cb(result);
-		});
-	}
+    deleteBurger: (condition, cb) => {
+        orm.deleteOne("burgers", condition, result => {
+            cb(result);
+        })
+    }
 };
 
 module.exports = burger;
