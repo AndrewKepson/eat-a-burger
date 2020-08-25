2758bc69eaaa71b58c35fcaa361e650ae602f3c1
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-25 14:25:59 -0600

    Updated formatting for clarity

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+const exphbs = require("express-handlebars");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -11,8 +10,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -22,8 +19,7 @@ const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// Start server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
