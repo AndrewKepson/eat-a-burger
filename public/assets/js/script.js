998fa5d538ad62a55e188b6078b71b424a1737e4
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-25 17:54:29 -0600

    Added Ajax calls

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,35 +1,39 @@
-$("body").on("click", ".eatButton", function () {
-  const id = $(this).data("id");
-  $.ajax({
-      type: "PUT",
-      url: `/api/burgers/${id}`,
-      data: {
-          devoured: 1
-      }
-  }).then(() => {
-      location.reload();
-  })
-})
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  console.log(newDevoured)
+      var newDevourState = {
+        devoured: newDevoured
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed sleep to", newDevoured);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
 
-$("body").on("click", ".deleteButton", function () {
-  const id = $(this).data("id");
-  $.ajax({
-      type: "DELETE",
-      url: `/api/burgers/${id}`
-  }).then(() => {
-      location.reload();
-  })
-})
-
-$("body").on("click", "#addButton", function () {
-  const text = $("#addABurgerText").val();
-  $.ajax({
-      type: "POST",
-      url: "/api/burgers",
-      data: {
-          burger_name: text
-      }
-  }).then(() => {
-      location.reload();
-  })
-})
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    });
+  });
