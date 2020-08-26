07c7472d81fd0d1943e1aadd7d309681b16fa7e4
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-25 18:27:57 -0600

    Set global script src for app deployment.

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const exphbs = require("express-handlebars");
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Set Up Middleware
 app.use(express.static("public"));
-
-// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+
 const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
